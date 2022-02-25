0e54a3cc81628df955d5558b434ee5a4e8dff65b
William Lee <leewp8@gmail.com>
2022-02-25 16:37:12 -0500

    installed yup and added submit button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Switch, Route, Link} from 'react-router-dom'
 import OrderForm from "./Components/OrderForm";
 
 const App = () => {
+
+  const [orders, setOrders] = useState([])
+
+  const orderSubmit = (newOrder) => {
+    setOrders([...orders, newOrder])
+  }
+
+  useEffect(() => {
+    console.log(orders)
+  }, [orders])
+
   return (
     <div className='App'>
       <header>
         <Link to="/">Home</Link>
-        
       </header>
       <Switch>
         <Route exact path="/">
@@ -16,8 +26,7 @@ const App = () => {
           <Link id="order-pizza" to="/pizza">Order Pizza</Link>
         </Route>
         <Route exact path="/pizza">
-          <OrderForm/>
-          
+          <OrderForm orderSubmit={orderSubmit}/>
         </Route>
       </Switch>
       
--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -1,60 +1,79 @@
 import React, {useState, useEffect} from 'react'
 
-const OrderForm = () => {
+const initialForm = {
+    name: '',
+    size: '',
+    pepperoni: false,
+    sausage: false,
+    mushroom: false,
+    onion: false,
+    special: '',
+    accepted: false
+}
 
-    const [form, setForm] = useState({
-        name: '',
-        size: '',
-        pepperoni: false,
-        sausage: false,
-        mushroom: false,
-        onion: false,
-        special: '',
-        accepted: false
-    })
+const OrderForm = (props) => {
+
+    // console.log(props)
+
+    const {orderSubmit} = props
+
+    const [form, setForm] = useState(initialForm)
 
     const formChange = (e) => {
-        console.log(e.target.name, e.target.value, e.target.checked)
+        // console.log(e.target.name, e.target.value, e.target.checked)
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        setForm({...form, [e.target.name]:value})
     }
 
+    const submitForm = (e) => {
+        e.preventDefault()
+        // console.log(form)
+        orderSubmit(form)
+        setForm(initialForm)
+    }
+
+    useEffect(() => {
+        // console.log(form)
+    }, [form] )
 
     return(<>
         <h1>Program your pizza!</h1>
-        <form id="pizza-form">
+        <form id="pizza-form" onSubmit={submitForm}>
             <label>
                 Enter your name
                 <input onChange={formChange} type='text' name='name' id='name-input' value={form.name} />
             </label>
             <label>
                 Select your size
-                <select name='size' id='size-dropdown' value={form.size}>
+                <select onChange={formChange} name='size' id='size-dropdown' value={form.size}>
                     <option value='small'>Small</option>
                     <option value='medium'>Medium</option>
                     <option value='large'>Large</option>
                 </select>
             </label>
-            <div> Toppings(as many as you want!):
+            <div> Toppings:
                 <label>Pepperoni
-                    <input onChange={formChange} type='checkbox' name='pepperoni' value={form.pepperoni} checked={form.pepperoni} />
+                    <input onChange={formChange} type='checkbox' name='pepperoni' checked={form.pepperoni} />
                 </label>
                 <label>Sausage
-                    <input onChange={formChange} type='checkbox' name='sausage' value={form.sausage} checked={form.sausage} />
+                    <input onChange={formChange} type='checkbox' name='sausage' checked={form.sausage} />
                 </label>
                 <label>Mushroom
-                    <input onChange={formChange} type='checkbox' name='mushroom' value={form.mushroom} checked={form.mushroom} />
+                    <input onChange={formChange} type='checkbox' name='mushroom' checked={form.mushroom} />
                 </label>
                 <label>Onion
-                    <input onChange={formChange} type='checkbox' name='onion' value={form.onion} checked={form.onion} />
+                    <input onChange={formChange} type='checkbox' name='onion' checked={form.onion} />
                 </label>
             </div>
             <label>
                 Special Instructions:
-                <input onChange={formChange} type='text' name='special' id='special-text' />
+                <input onChange={formChange} type='text' name='special' id='special-text' value={form.special} />
             </label>
-            <label>
+            {/* <label>
                 Ready to bake!
                 <input onChange={formChange} type='checkbox' name='accepted' value={form.accepted} />
-            </label>
+            </label> */}
+            <button type='submit' id='order-button'>Add to Order</button>
         </form>
         </>)
 
